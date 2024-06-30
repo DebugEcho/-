ca51b7d278af7f5b29dbfd3873db1a7433afa4e1
debuged <debuged@outlook.com>
2024-06-30 17:41:55 +0800

    feat(工厂模式): 添加工厂模式和工厂方法模式的实现
    在src/main/java/cn/debuged/factory目录下新增了Factory.java文件，包含了简单工厂模式的实现。在src/main/java/cn/debuged/factory/r目录下新增了FactoryMethod.java文件，包含了工厂方法模式的实现。
    这两个模式都是创建型设计模式，用于生产特定类型的对象。
    
    Factory.java:
    - 实现了一个简单的工厂类用于根据类型创建不同的操作（加、减、乘、除）。
    - 该文件中注释掉的部分是工厂方法模式的实现框架。
    
    FactoryMethod.java:- 展示了工厂方法模式的使用，其中Factory接口定义了创建产品的方法，FactoryA和FactoryB实现了该接口并分别生产ProductA和ProductB。
    - Product接口定义了产品的行为，ProductA和ProductB实现了该接口。
    
    这些代码示例有助于理解工厂模式和工厂方法模式的概念和应用。

---
+++ src/main/java/cn/debuged/factory/Factory.java
@@ -0,0 +1,28 @@
+package cn.debuged.factory;
+
+import com.sun.org.apache.xpath.internal.operations.Div;
+
+public class Factory {
+    public static void main(String[] args) {
+
+    }
+    // class OberationFactory {
+    //     public static Oberation create(String type) {
+    //         switch (type) {
+    //             case "+":
+    //                 return new Add();
+    //             case "-":
+    //                 return new Sub();
+    //             case "*":
+    //                 return new Mul();
+    //             case "/":
+    //                 return new Div();
+    //             default:
+    //                 return null;
+    //         }
+    //     }
+    // }
+    // interface IFactory {
+    //     public Oberation create();
+    // }
+}
