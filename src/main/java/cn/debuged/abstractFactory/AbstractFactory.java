75f3c426ad308eb273e59a86f35e66d391545fc8
jiangyewei <jiangyewei@rzon.tech>
2024-08-30 14:21:20 +0800

    refactor(AbstractFactory): 使用DataAccess替换工厂实例化
    将用户和部门的数据访问对象创建从具体的SqlServerFactory替换为DataAccess类。此更改允许在不同数据库之间更灵活地切换，而无需修改现有代码。
    
    DataAccess类根据当前数据库配置动态实例化适当的数据访问对象，支持Sqlserver和Access数据库。

--- src/main/java/cn/debuged/abstractFactory/AbstractFactory.java
+++ src/main/java/cn/debuged/abstractFactory/AbstractFactory.java
@@ -5,11 +5,15 @@ import lombok.Data;
 public class AbstractFactory {
     public static void main(String[] args) {
         User user = new User();
+        Department department = new Department();
         // SqlserverUser sqlserverUser = new SqlserverUser();
-        IFactory factory = new SqlServerFactory();
-        IUser iu = factory.createUser();
+
+        IUser iu = DataAccess.createUser();
         iu.insert(user);
         iu.getUser(1);
+        IDepartment idep = DataAccess.createDepartment();
+        idep.insert(department);
+        idep.getDepartment(1);
     }
 }
 
@@ -18,10 +22,23 @@ class User {
     private int id;
     private String name;
 }
+
+@Data
+class Department {
+    private int id;
+    private String name;
+}
+
 interface IUser {
     void insert(User user);
 
     User getUser(int id);
+}
+
+interface IDepartment {
+    void insert(Department department);
+
+    Department getDepartment(int id);
 }
 
 class SqlserverUser implements IUser {
@@ -35,6 +52,18 @@ class SqlserverUser implements IUser {
     }
 }
 
+class SqlserverDepartment implements IDepartment {
+    public void insert(Department department) {
+        System.out.println("在Access中给Department表增加一条记录");
+    }
+
+    public Department getDepartment(int id) {
+        System.out.println("在Access中根据ID得到Department表的一条记录");
+        return null;
+    }
+}
+
+
 class AccessUser implements IUser {
     public void insert(User user) {
         System.out.println("在AccessUser中给User表增加一条记录");
@@ -46,17 +75,72 @@ class AccessUser implements IUser {
     }
 }
 
+class AccessDepartment implements IDepartment {
+    public void insert(Department department) {
+        System.out.println("在Access中给Department表增加一条记录");
+    }
+
+    public Department getDepartment(int id) {
+        System.out.println("在Access中根据ID得到Department表的一条记录");
+        return null;
+    }
+}
 
 interface IFactory {
     public IUser createUser();
+
+    public IDepartment createDepartment();
 }
+
 class SqlServerFactory implements IFactory {
     public IUser createUser() {
         return new SqlserverUser();
     }
+
+    @Override
+    public IDepartment createDepartment() {
+        return new SqlserverDepartment();
+    }
 }
+
 class AccessFactory implements IFactory {
     public IUser createUser() {
         return new AccessUser();
+    }
+
+    @Override
+    public IDepartment createDepartment() {
+        return new AccessDepartment();
+    }
+}
+class DataAccess{
+    private static String db = "SqlServer";
+    static IUser createUser(){
+        IUser result = null;
+        switch (db){
+            case "SqlServer":
+                result = new SqlserverUser();
+                break;
+            case "Access":
+                result = new AccessUser();
+                break;
+            default:
+                break;
+        }
+        return result;
+    }
+    static IDepartment createDepartment(){
+        IDepartment result = null;
+        switch (db){
+            case "SqlServer":
+                result = new SqlserverDepartment();
+                break;
+            case "Access":
+                result = new AccessDepartment();
+                break;
+            default:
+                break;
+        }
+        return result;
     }
 }
